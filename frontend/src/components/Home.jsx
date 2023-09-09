ba4693bee5e83cd9a1948e2dac8347e7d033fb65
Jeff Bermejo <77440578+jef-design@users.noreply.github.com>
2023-09-09 10:50:46 +0800

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     const dispatch = useDispatch()
 
     useEffect(() => {
-      axios.get('https://blog-api-drab-delta.vercel.app')
+      axios.get('https://blog-api-drab-delta.vercel.app/api/blog')
         .then(response => {
           dispatch(setAllBlogs(response.data))
         })
